91c4b1f031288b1913d4681e7188df4dbe0cc7c5
Eon Pace <nicolaspace21@gmail.com>
2024-08-06 03:16:37 -0400

    finishes server and notes.json

---
+++ Develope/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { clog } = require('./midware/clog');
+const routes = require('./routes')
+
+const PORT = 3001;
+const app = express();
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use(express.static('public'));
+
+app.use(routes);
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+  );
