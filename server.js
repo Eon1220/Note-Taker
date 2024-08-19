3ca1d528c4710df6af1d3b8ff3d47431df1a15e9
Eon1220 <Nicolaspace21@gmail.com>
2024-08-19 14:42:15 -0400

    it was over a capital

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.static('public'));
 app.use('/api', api);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, './Public/index.html'))
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, './Public/notes.html'))
 });
 
 
